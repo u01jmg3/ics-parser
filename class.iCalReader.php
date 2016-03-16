6085a18242eaf98e6586e3defa7705fa207847ac
Jonathan Goode <u01jmg3@gmail.com>
2016-03-16 13:31:55 +0000

    Version bump

--- class.iCalReader.php
+++ class.iCalReader.php
@@ -11,7 +11,7 @@
  * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  * @link     https://github.com/MartinThoma/ics-parser/
  * @example  $ical = new ical('MyCal.ics'); print_r($ical->events());
- * @version  1.0.2
+ * @version  1.0.3
  */
 
 /**
