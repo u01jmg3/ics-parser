b0e51b6ba2d5e28583387480835b6632b9d91f9a
Jonathan Goode <u01jmg3@gmail.com>
2018-10-19 11:10:58 +0100

    Version bump

--- src/ICal/ICal.php
+++ src/ICal/ICal.php
@@ -7,7 +7,7 @@
  *
  * @author  Jonathan Goode <https://github.com/u01jmg3>
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 2.1.4
+ * @version 2.1.6
  */
 
 namespace ICal;
