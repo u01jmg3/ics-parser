0bb320e12c3079490f33911fe07bb7625a147ad6
Jonathan Goode <u01jmg3@gmail.com>
2020-11-16 14:55:37 +0000

    Lowercase

--- examples/index.php
+++ examples/index.php
@@ -24,7 +24,7 @@ try {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>PHP ICS Parser example</title>
