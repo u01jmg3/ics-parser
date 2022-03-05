b82c779853f02cce1a676e2ecf315ba08b511674
u01jmg3 <u01jmg3@gmail.com>
2022-03-05 15:12:37 +0000

    Correct version

--- src/ICal/ICal.php
+++ src/ICal/ICal.php
@@ -8,7 +8,7 @@
  *
  * @author  Jonathan Goode <https://github.com/u01jmg3>
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 2.2.2
+ * @version 3.1.0
  */
 
 namespace ICal;
